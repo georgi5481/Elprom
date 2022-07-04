d01fdd5f2cf2e4310da3104a74357fc26ca2ace9
George <georgi5481@abv.bg>
2022-07-04 22:24:31 +0300

    changed the input save logic

--- FileReader.cpp
+++ FileReader.cpp
@@ -52,7 +52,7 @@ FileReader::FileReader(std::string& path){
 					std::string otherHalf;
 					std::getline(streamFileReader,otherHalf, separatingCharacter);
 					input.material += otherHalf;
-				}
+					}
 				std::getline(streamFileReader, input.weigthSingleDetail , separatingCharacter);
 
 				std::getline(streamFileReader, input.BTES);
@@ -66,12 +66,14 @@ FileReader::FileReader(std::string& path){
 						input.material << " , " << input.weigthSingleDetail << " , " << input.BTES << std::endl << savings::savedAllInputs.size() << std::endl;
 
 			}
+
+
+			streamFileReader.close();
 	 }
 	else{
 		  std::cerr << "Could not open the file location : " << path << std::endl;
 	}
 
-	streamFileReader.close();
 
 }
 
@@ -98,34 +100,35 @@ double FileReader::turnIntoDouble(std::string& turnIntoDouble){
 
 void FileReader::singleObjectSort(DataSaver& storedDataLine){
 
-	if(storedDataLine.positionNumber != " "){
+	if(storedDataLine.positionNumber != " " ){
 
 		double weightMaterial = turnIntoDouble(storedDataLine.weigthSingleDetail);	//set the weight to zero by dafault
+			if(weightMaterial != 0) {
+				double quantity = turnIntoDouble(storedDataLine.quantity);
 
-		double quantity = turnIntoDouble(storedDataLine.quantity);
+				storedDataLine.quantityDouble = quantity;
+				storedDataLine.weightSingleDetailDouble = weightMaterial;
 
-		storedDataLine.quantityDouble = quantity;
-		storedDataLine.weightSingleDetailDouble = weightMaterial;
+				if(storedDataLine.material.find("C2R") != std::string::npos | storedDataLine.material.find("P4R") != std::string::npos){
+					savings::savedWood.push_back(storedDataLine);
 
-		if(storedDataLine.material.find("C2R") != std::string::npos || storedDataLine.material.find("P4R") != std::string::npos){
-			savings::savedWood.push_back(storedDataLine);
-
-		}
-		else if(storedDataLine.material.find("Cu") != std::string::npos || storedDataLine.material.find("мед") != std::string::npos
-				|| storedDataLine.nameDetail.find("отвод") != std::string::npos){
-			savings::savedCu.push_back(storedDataLine);
-		}
-		else if(storedDataLine.material.find("Etronit") != std::string::npos ){
-			savings::savedEtronit.push_back(storedDataLine);
 				}
-		else if(storedDataLine.material.find("Трафоборд") != std::string::npos ){
-			savings::savedTransformerboard.push_back(storedDataLine);
-		}
-		else{
-			savings::savedUnknownData.push_back(storedDataLine);
+				else if(storedDataLine.material.find("Cu") != std::string::npos || storedDataLine.material.find("мед") != std::string::npos
+						|| storedDataLine.nameDetail.find("отвод") != std::string::npos){
+					savings::savedCu.push_back(storedDataLine);
+				}
+				else if(storedDataLine.material.find("Etronit") != std::string::npos ){
+					savings::savedEtronit.push_back(storedDataLine);
+						}
+				else if(storedDataLine.material.find("Трафоборд") != std::string::npos ){
+					savings::savedTransformerboard.push_back(storedDataLine);
+				}
+				else{
+					savings::savedUnknownData.push_back(storedDataLine);
+				}
+			}
 		}
 	}
-}
 
 
 void FileReader::saveFilledFile(){
@@ -147,41 +150,44 @@ void FileReader::printSavedData(std::string& nameOfOutputFile){
 	 std::ofstream outputStream;
 	 std::string pathOutput = "./Mass/" + nameOfOutputFile;
 
-	 outputStream.open(pathOutput);
+	 if(savings::savedAllInputs.size() != 0){
 
-	 if(savings::savedWood.size() != 0){
-		 outputStream << "Found wood on position:\n";
-		 fillOutput(savings::savedWood, outputStream);
-		 outputStream << std::endl;
+		 outputStream.open(pathOutput);
+
+			 if(savings::savedWood.size() != 0){
+				 outputStream << "Found wood on position:\n";
+				 fillOutput(savings::savedWood, outputStream);
+				 outputStream << std::endl;
+			 }
+
+			 if(savings::savedCu.size() != 0){
+				 outputStream << "Found Cu on position:\n";
+				 fillOutput(savings::savedCu, outputStream);
+				 outputStream << std::endl;
+			 }
+
+			 if(savings::savedTransformerboard.size() != 0){
+				 outputStream << "Found Transformerboard on position:\n";
+				 fillOutput(savings::savedTransformerboard, outputStream);
+				 outputStream << std::endl;
+			 }
+
+
+			 if(savings::savedEtronit.size() != 0){
+				 outputStream << "Found Etronit on position:\n";
+				 fillOutput(savings::savedEtronit, outputStream);
+				 outputStream << std::endl;
+			 }
+
+
+			 if(savings::savedUnknownData.size() != 0){
+				 outputStream << "Unknown material on position:\n";
+				 fillOutput(savings::savedUnknownData, outputStream);
+				 outputStream << std::endl;
+			 }
+
+		 outputStream.close();
 	 }
-
-	 if(savings::savedCu.size() != 0){
-		 outputStream << "Found Cu on position:\n";
-		 fillOutput(savings::savedCu, outputStream);
-		 outputStream << std::endl;
-	 }
-
-	 if(savings::savedTransformerboard.size() != 0){
-		 outputStream << "Found Transformerboard on position:\n";
-		 fillOutput(savings::savedTransformerboard, outputStream);
-		 outputStream << std::endl;
-	 }
-
-
-	 if(savings::savedEtronit.size() != 0){
-		 outputStream << "Found Etronit on position:\n";
-		 fillOutput(savings::savedEtronit, outputStream);
-		 outputStream << std::endl;
-	 }
-
-
-	 if(savings::savedUnknownData.size() != 0){
-		 outputStream << "Unknown material on position:\n";
-		 fillOutput(savings::savedUnknownData, outputStream);
-		 outputStream << std::endl;
-	 }
-
- 	 outputStream.close();
 
 }
 
